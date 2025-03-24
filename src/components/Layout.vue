ee16ee13c8284316c1b69f3f8f4590f1ba572ed6
rodrigojmayer <rodrigojmayer@gmail.com>
2025-03-24 17:51:15 -0300

    progress open country details

---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// import { useTheme } from 'vuetify'
+
+
+const props = defineProps<{
+    colorMode: string
+}>()
+const emit = defineEmits<{
+    (e: 'update:colorMode', value: string): void
+}>()
+
+</script>
+
+<template>
+    <!-- <div :class="['head', `head-${colorMode}`]"> -->
+    <div :class="['head']">
+        <h1>Where in the world?</h1>
+
+        
+        <button @click="emit('update:colorMode', props.colorMode === 'myCustomLightTheme' ? 'myCustomDarkTheme' : 'myCustomLightTheme')">
+            <span v-if="props.colorMode === 'myCustomLightTheme'">🌙</span>
+            <span v-else>☀️</span>
+            {{ props.colorMode === 'myCustomLightTheme' ? 'Dark Mode' : 'Light Mode' }}
+        </button>
+    
+    </div>
+</template>
+
+<style scoped>
+
+</style>
